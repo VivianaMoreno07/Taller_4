b5f5f9a9f5b5013f103a4f9d4a9fec7e358304c5
Viviana Moreno <vmorenoo@udistrital.edu.co>
2024-04-10 15:44:00 -0500

    Numpy taller

--- Programa.py
+++ Programa.py
@@ -1,8 +1,10 @@
 import numpy as np
+#Tenemos que importar matplotlib, que es una biblioteca especializada mas que todo en graficos
 import matplotlib.pyplot as plt
+#Se define la funcion Matplotlib con las tres variables que se van a trabajar en el codigo
 def mandelbrot(h, w, maxit=20, r=2):
     """Returns an image of the Mandelbrot fractal of size (h,w)."""
-    x = np.linspace(-2.5, 1.5, 4*h+1)
+    x = np.linspace(-2.5, 1.5, 4*h+1) # Se crea un rango de numeros entre -2.5 y 1.5 y con eso se crea una lista con el espacio definido 
     y = np.linspace(-1.5, 1.5, 3*w+1)
     A, B = np.meshgrid(x, y)
     C = A + B*1j
@@ -11,10 +13,10 @@ def mandelbrot(h, w, maxit=20, r=2):
 
     for i in range(maxit):
         z = z**2 + C
-        diverge = abs(z) > r                    # who is diverging
-        div_now = diverge & (divtime == maxit)  # who is diverging now
-        divtime[div_now] = i                    # note when
-        z[diverge] = r                          # avoid diverging too much
+        diverge = abs(z) > r                   
+        div_now = diverge & (divtime == maxit)  
+        divtime[div_now] = i                    
+        z[diverge] = r                          
 
     return divtime
 plt.clf()
